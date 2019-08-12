4bff14fc0627709c1f5811dfa2c4ea32e51a08ce
juniorpires <juniorpiresupe@gmail.com>
2019-08-12 13:32:57 -0300

    adicionado interface javascript

--- app/src/main/java/com/github/juniorpires/insidewebview/ui/MainActivity.java
+++ app/src/main/java/com/github/juniorpires/insidewebview/ui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.github.juniorpires.insidewebview.ui;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -16,11 +19,13 @@ import tools.AndroidUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String URL = "https://www.google.com.br/";
+    public static final String URL = "https://suporte.pointmaquinetas.com.br";
 
     View view;
 
     private WebView myWebView;
+    public static String barcode = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        myWebView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
 
 
         init();
@@ -94,4 +101,36 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                //here is where you get your result
+                barcode = intent.getStringExtra("SCAN_RESULT");
+            }
+        }
+    }
+
+    public class JavaScriptInterface {
+        Context mContext;
+
+        // Instantiate the interface and set the context
+        JavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        // using Javascript to call the finish activity
+        public void closeMyActivity() {
+            finish();
+        }
+
+        @JavascriptInterface
+        public void scanBarcode() {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.setPackage("com.google.zxing.client.android");
+            startActivityForResult(intent, 0);
+        }
+    }   //JavascriptInterface
 }
+
+
